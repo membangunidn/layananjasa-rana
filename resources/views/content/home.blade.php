17d9f64b360e0127821830238447de7e167469f4
Ma'sum <muhmasum6661@gmail.com>
2021-09-14 16:15:57 +0700

    rewrite

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -8,6 +8,40 @@
 
 @section('content')
 
+<section class="section" id="contactus" sytle="margin-top:-120px; !important">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6"></div>
+            <div class="col-md-6 align-self-center">
+                <div class="left-text-content">
+                    <div class="section-heading">
+                        <h6>Contact Us</h6>
+                        <h2>Here You Can Make A contactus Or Just walkin to our cafe</h2>
+                    </div>
+                    <p>Donec pretium est orci, non vulputate arcu hendrerit a. Fusce a eleifend riqsie, namei sollicitudin urna diam, sed commodo purus porta ut.</p>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="phone">
+                                <i class="fa fa-phone"></i>
+                                <h4>Phone Numbers</h4>
+                                <span><a href="#">080-090-0990</a><br><a href="#">080-090-0880</a></span>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="message">
+                                <i class="fa fa-envelope"></i>
+                                <h4>Emails</h4>
+                                <span><a href="#">hello@company.com</a><br><a href="#">info@company.com</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
 <section class="container">
     <div class="landing">
         <div class="flex flex-row justify-content-center flex-center">
